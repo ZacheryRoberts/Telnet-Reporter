a4013565ca34ffb67fd879283d3fb2d018e460cb
Zachery Roberts <42316926+ZacheryRoberts@users.noreply.github.com>
2022-03-16 19:19:23 +1100

    Simplified code, improved readabillity

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from posixpath import split
 import telnetlib
 import ipaddress
 import concurrent.futures
@@ -12,45 +13,50 @@ def telnet_test(network):
             tn_read = tn.read_until(b": ", timeout=4)
             if tn_read != "":
                     output.append(str(ip))
-            print(f"{ip} Telnet Accepted")
+                    print(f"{ip} Telnet Accepted")
         except:
             print(f"{ip} Telnet Refused or Host Not Available")
     return(output)
+
+# Gets network, for readability 
+def get_network(network, cidr):
+    return (ipaddress.IPv4Network(f"{network}/{cidr}"))
 
 
 # Test User input, create multi processes, then find addresses
 if __name__ == "__main__":
     telnetHosts = []
-    splitAddress = []
+    addressSplit = []
     networkSplit = []
     try:
-        # User Input
-        address = input("Network Addr: ")
-        cidr = int(input("Netmask (CIDR): "))
+    # User Input
+        address, cidr = input("Network Addr: "), int(input("Netmask (CIDR): "))
 
         # Split Subnets for Multi Processing
-        speed = min(max(int(input("Speed (1-4): ")), 1), 4)        
+        speed = min(max(int(input("Speed (1-8): ")), 1), 8)        
         cidrSplit = min(max(speed+cidr, 0), 32)
-        splitAddress.append(ipaddress.IPv4Address(f"{address}"))
-        for i in range(1, (speed*speed)):
-            splitAddress.append(list(ipaddress.IPv4Network(f"{splitAddress[i-1]}/{cidrSplit}").hosts())[-1] + 2)  
-        for i in range(len(splitAddress)):
-            networkSplit.append(ipaddress.IPv4Network(f"{splitAddress[i]}/{cidrSplit}"))
 
+        networkSplit.append(get_network(address, cidrSplit))
+        for i in range(1, (speed*speed)):
+            temp = list(ipaddress.IPv4Network(f"{networkSplit[i-1]}").hosts())[-1] + 2
+            networkSplit.append(get_network(temp, cidrSplit))
+        
        # Mutli Process Creation
         with concurrent.futures.ProcessPoolExecutor() as executor:
             results = [executor.submit(telnet_test, networkSplit[i]) for i in range(len(networkSplit))]
             for f in concurrent.futures.as_completed(results):
                 telnetHosts.append(f.result())
         
-        telnetFlatten = [item for sublist in telnetHosts for item in sublist]
-        print(f"Hosts with Telnet\n{sorted(telnetFlatten)}")
+        # Flatten lists, print sorted addresses
+        telnetFlatten = sorted([item for sublist in telnetHosts for item in sublist])
+        print(f"Hosts with Telnet\n{telnetFlatten}")
 
-        
+        # Save results
         if input("Save results? Y/N: ").upper == 'Y':
             with open(f"telnetfinder-{date.today()}.txt", "w") as output:
-                output.write(str(telnetFlatten))      
-     
+                output.write(str(telnetFlatten))
+
     except ValueError:
-         print('address/netmask is invalid for IPv4:', address, cidr)
- 
+        print('address/netmask is invalid for IPv4:', address, cidr)
+   
+    
